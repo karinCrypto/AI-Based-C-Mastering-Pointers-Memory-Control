b490b096b53817eb2403649f36942fb3c02478af
Two line Dev <129065300+karinCrypto@users.noreply.github.com>
2025-11-20 21:49:52 +0900

    Create ErrorHandling.cpp

---
+++ ErrorHandling.cpp
@@ -0,0 +1,10 @@
+fn read_file(path: &str) -> Result<String, std::io::Error> {
+    std::fs::read_to_string(path)
+}
+
+fn main() {
+    match read_file("data.txt") {
+        Ok(content) => println!("{}", content),
+        Err(e) => println!("Error: {}", e),
+    }
+}
